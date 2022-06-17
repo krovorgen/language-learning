3888fa86c85f45e8ec030a991af7f39b196e1197
maksim <krovorgen@gmail.com>
2022-06-17 08:12:17 +0300

    fixed scss

--- lib/api/dictionary.ts
+++ lib/api/dictionary.ts
@@ -1,15 +1,29 @@
 import clientPromise from '../mongodb';
 
-type DataType = { lang: 'eng'; word: string; translation: string; point: number };
+export type DictionaryType = {
+  id: number;
+  lang: 'eng';
+  word: string;
+  translation: string;
+  point: number;
+};
+
+export type CreateBodyType = {
+  lang: 'eng';
+  word: string;
+  translation: string;
+};
 
 export async function getDictionary(): Promise<any> {
   const client = await clientPromise;
-  const collection = client.db('test').collection<DataType>('dictionary');
+  const collection = client.db('test').collection<DictionaryType>('dictionary');
   return collection.find({}, { projection: { _id: 0 } }).toArray();
 }
-export async function createDictionary(body: DataType): Promise<any> {
+export async function createDictionary(body: CreateBodyType): Promise<any> {
   const client = await clientPromise;
-  const collection = client.db('test').collection<DataType>('dictionary');
+  const collection = client.db('test').collection<DictionaryType>('dictionary');
 
-  return await collection.insertOne(body);
+  const data: DictionaryType = { ...body, id: +new Date(), point: 0 };
+
+  return await collection.insertOne(data);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,36 @@
 import { NextPage } from 'next';
-import { Button } from '@alfalab/core-components/button';
-import { getDictionary } from '@/lib/api/dictionary';
+import { DictionaryType, getDictionary } from '@/lib/api/dictionary';
+import { Table } from '@alfalab/core-components/table';
 
-const Home: NextPage = ({ dictionary }: any) => {
-  console.log(dictionary);
+type Props = {
+  dictionary: DictionaryType[];
+};
+
+enum DictionaryLanguage {
+  'eng' = 'Английский',
+}
+
+const Home: NextPage<Props> = ({ dictionary }) => {
   return (
-    <div>
-      <Button>Hello world</Button>
-    </div>
+    <>
+      <h1>11</h1>
+      <Table>
+        <Table.THead>
+          <Table.THeadCell title="Язык">Язык</Table.THeadCell>
+          <Table.THeadCell title="Слово">Слово</Table.THeadCell>
+          <Table.THeadCell title="Перевод">Перевод</Table.THeadCell>
+        </Table.THead>
+        <Table.TBody>
+          {dictionary.map((row) => (
+            <Table.TRow key={row.id}>
+              <Table.TCell>{DictionaryLanguage[row.lang]}</Table.TCell>
+              <Table.TCell>{row.word}</Table.TCell>
+              <Table.TCell>{row.translation}</Table.TCell>
+            </Table.TRow>
+          ))}
+        </Table.TBody>
+      </Table>
+    </>
   );
 };
 
