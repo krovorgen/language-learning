28924e520c094d41b0c90dd234721aae66c4cf0f
maksim <krovorgen@gmail.com>
2022-06-18 16:55:57 +0300

    small refactor

--- src/components/LevelStudy/LevelStudy.tsx
+++ src/components/LevelStudy/LevelStudy.tsx
@@ -7,14 +7,15 @@ type Props = {
   point: number;
 };
 
-export const LevelStudy: FC<Props> = memo(({ point }) => {
-  const classView = {
-    [styles.low]: point < 10,
-    [styles.medium]: point >= 10 && point < 20,
-    [styles.high]: point >= 20 && point < 30,
-    [styles.perfect]: point >= 30,
-  };
-  return <span className={cn(styles.root, classView)} />;
-});
+export const LevelStudy: FC<Props> = memo(({ point }) => (
+  <span
+    className={cn(styles.root, {
+      [styles.low]: point < 10,
+      [styles.medium]: point >= 10 && point < 20,
+      [styles.high]: point >= 20 && point < 30,
+      [styles.perfect]: point >= 30,
+    })}
+  />
+));
 
 LevelStudy.displayName = 'LevelStudy';
