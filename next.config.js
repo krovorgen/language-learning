2ca88ed6bbc60239ee2aa4d4a65030014e2098a1
maksim <krovorgen@gmail.com>
2022-06-26 11:07:10 +0300

    test next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const { withGlobalCss } = require('next-global-css');
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const withConfig = withGlobalCss();
 
-module.exports = withConfig({
+module.exports = withPlugins([withConfig, withBundleAnalyzer], {
   reactStrictMode: true,
 });
