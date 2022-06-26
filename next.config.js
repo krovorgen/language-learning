97a284603aa7d09e45718dca8375f80817001ad5
maksim <krovorgen@gmail.com>
2022-06-26 11:17:44 +0300

    del bundle analyze

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const { withGlobalCss } = require('next-global-css');
 const withPlugins = require('next-compose-plugins');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
 
-const withConfig = withGlobalCss();
-
-module.exports = withPlugins([withConfig, withBundleAnalyzer], {
+module.exports = withPlugins([withGlobalCss()], {
   reactStrictMode: true,
 });
