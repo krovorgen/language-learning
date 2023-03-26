c8dc72388f828ef8dac58e2ec6051844e4e4d4aa
maksim <krovorgen@gmail.com>
2023-03-26 17:42:14 +0300

    изменение скорости

--- src/helpers/wordVoiceActing.ts
+++ src/helpers/wordVoiceActing.ts
@@ -8,6 +8,7 @@ enum LangISO {
 export const wordVoiceActing = (word: string, lang: DictionaryLangType) => {
   const msg = new SpeechSynthesisUtterance();
   msg.text = word;
+  msg.rate = 0.8;
   msg.lang = LangISO[lang];
   window.speechSynthesis.speak(msg);
 };
